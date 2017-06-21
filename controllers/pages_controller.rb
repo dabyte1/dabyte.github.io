dfcc783ffbbd157cf9878c908aa399d36c148de2
Daniel M <mogosdanielmihai@gmail.com>
2017-06-21 15:13:56 +0300

    Add files via upload

---
+++ controllers/pages_controller.rb
@@ -0,0 +1,19 @@
+class PagesController < ApplicationController
+  def HOME
+  end
+
+  def SERVICII
+  end
+
+  def PARTENERI
+  end
+
+  def CARIERE
+  end
+
+  def DESPRE
+  end
+
+  def CONTACT
+  end
+end
---
+++ mailers/application_mailer.rb
@@ -0,0 +1,4 @@
+class ApplicationMailer < ActionMailer::Base
+  default from: "dani.optimustransport@gmail.com"
+  layout 'mailer'
+end
